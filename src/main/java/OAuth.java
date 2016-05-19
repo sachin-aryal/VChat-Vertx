12b260ca9dfd5f27a99d7a7c7d593ad6bfd1b094
sachin-aryal <saaryal51@gmail.com>
2016-05-19 07:22:36 +0545

    Login Successfull

--- src/main/java/OAuth.java
+++ src/main/java/OAuth.java
@@ -1,20 +1,20 @@
+import io.vertx.core.json.JsonObject;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
+import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 
-/**
- * Author: SACHIN
- * Date: 5/6/2016.
- */
 public class OAuth {
-    private final String consumerKey = "8n4Fm4FVUX289HbhwIavKV59V";
-    private final String consumerSecret = "vVkl6RfKPEgsff3Kz8SngDOaQ9gDfmnyk93EPwwmpJJgffhrOw";
+    private final static String consumerKey = "8n4Fm4FVUX289HbhwIavKV59V";
+    private final static String consumerSecret = "vVkl6RfKPEgsff3Kz8SngDOaQ9gDfmnyk93EPwwmpJJgffhrOw";
 
-    public static String getAuthorizationUrl(){
+    public static JsonObject getAuthorizationUrl() {
         Twitter twitter = TwitterFactory.getSingleton();
         String returnUrl = "http://localhost:8080/result";
         twitter.setOAuthConsumer(consumerKey, consumerSecret);
+        JsonObject jsonObject = new JsonObject();
         String authorizationURL = null;
         try {
             RequestToken requestToken = twitter.getOAuthRequestToken(returnUrl);
@@ -22,6 +22,9 @@ public class OAuth {
                 String tokenKeyGen = requestToken.getToken();
                 String tokenSecretKeyGen = requestToken.getTokenSecret();
                 authorizationURL = requestToken.getAuthorizationURL();
+                jsonObject.put("tokenKey",tokenKeyGen);
+                jsonObject.put("tokenKeySecret",tokenSecretKeyGen);
+                jsonObject.put("authorizationUrl",authorizationURL);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -29,6 +32,29 @@ public class OAuth {
         } catch (TwitterException e) {
             e.printStackTrace();
         }
-        return authorizationURL;
+        return jsonObject;
+    }
+
+    public static User oResult(String tokenKeyGen,String tokenSecretKeyGen,String verifier) {
+        System.out.println("Redirecting to the logged in url");
+
+        Twitter twitter = new TwitterFactory().getInstance();
+
+        twitter.setOAuthConsumer(consumerKey, consumerSecret);
+//        String verifier = routingContext.request().getParam("oauth_verifier");
+        RequestToken requestToken = new RequestToken(tokenKeyGen, tokenSecretKeyGen);
+        AccessToken accessToken = null;
+        User user = null;
+        try {
+            accessToken = twitter.getOAuthAccessToken(requestToken, verifier);
+            twitter.setOAuthAccessToken(accessToken);
+            user = twitter.verifyCredentials();
+
+        } catch (TwitterException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 }
+
+
--- src/main/java/VChat.java
+++ src/main/java/VChat.java
@@ -3,6 +3,7 @@ import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.StaticHandler;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
@@ -14,18 +15,17 @@ import io.vertx.ext.web.handler.sockjs.SockJSHandler;
  */
 public class VChat extends AbstractVerticle{
 
-    private final String consumerKey = "8n4Fm4FVUX289HbhwIavKV59V";
-    private final String consumerSecret = "vVkl6RfKPEgsff3Kz8SngDOaQ9gDfmnyk93EPwwmpJJgffhrOw";
+    private final String twitterUrl = "https://api.twitter.com/oauth/authorize?oauth_token=hDz27wAAAAAAu6pmAAABVMGW3c0";
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(new VChat());
     }
 
-    public void start(){
+    public void start() {
         Router router = Router.router(vertx);
         vertx.createHttpServer().requestHandler(router::accept).listen(8080);
-        router.route("/").handler(rtx->{
+        router.route("/").handler(rtx -> {
             HttpServerResponse response = rtx.response();
             response.setChunked(true);
             response.sendFile("webroot/index.html");
@@ -37,25 +37,44 @@ public class VChat extends AbstractVerticle{
 
         SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
         BridgeOptions options = new BridgeOptions();
-        options.addInboundPermitted(new PermittedOptions().setAddress("client-to-server"));
-        options.addOutboundPermitted(new PermittedOptions().setAddress("server-to-client"));
+        options.addInboundPermitted(new PermittedOptions().setAddress("chat.to.server"));
+        options.addInboundPermitted(new PermittedOptions().setAddress("chat.to.server.userInfo"));
+        options.addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client"));
+        options.addOutboundPermitted(new PermittedOptions().setAddress("chat.to.client.userInfo"));
         sockJSHandler.bridge(options);
 
 
         router.route("/loginUrl/*").handler(sockJSHandler);
+        router.route("/userInfo/*").handler(sockJSHandler);
+        router.route("/result/*").handler(this::resultCallback);
+        loginListner();
+        userInfo();
+    }
 
-        sockJSHandler.bridge(options);
+    public void loginListner(){
 
         EventBus eb = vertx.eventBus();
 
-        eb.consumer("client-to-server").handler(sockJSHand->{
-            System.out.println("Received Message "+sockJSHand.body());
+        eb.consumer("chat.to.server",message->{
+
             vertx.executeBlocking(future -> {
                 future.complete(OAuth.getAuthorizationUrl());
             }, res -> {
-                
+                eb.publish("chat.to.client",res.result());
             });
         });
+
+    }
+    public void userInfo(){
+        EventBus eb = vertx.eventBus();
+        eb.consumer("chat.to.server.userInfo",data->{
+
+        });
+    }
+    public void resultCallback(RoutingContext routingContext){
+        System.out.println("Calling");
+        String verifier = routingContext.request().getParam("oauth_verifier");
+
 
     }
 }
